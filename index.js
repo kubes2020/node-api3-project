97155ec81a1740447b90ad5550b448d8dd44e9a0
Brian Kubes <briankubes@gmail.com>
2020-10-29 13:16:28 -0700

    ready to push

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// code away!
+require('dotenv').config()
+
+const port = process.env.PORT || 4040
 const server = require('./server.js');
-server.listen(4040, () => {
-  console.log('\n* Server Running on http://localhost:4040 *\n');
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
